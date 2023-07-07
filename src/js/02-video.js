1489aa6be093cdcdafe2f837230b5530d54f4c12
OleksiiMinnikov <89975657+OleksiiMinnikov@users.noreply.github.com>
2023-07-07 17:54:39 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,20 @@
 import Player from '@vimeo/player';
-console.log(Player);
+// console.log(Player);
+
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+const storageKey = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime() {
+  player.getCurrentTime().then((seconds) => {
+    localStorage.setItem(storageKey, seconds);
+  });
+}
+
+const savedTime = localStorage.getItem(storageKey);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
